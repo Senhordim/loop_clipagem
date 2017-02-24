6cdf3da5dfc5340615fc6a2726b84a37e7d80bf8
Diego Collares <diego.collares@gmail.com>
2017-02-24 18:07:49 -0300

    created route to ad

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-  get 'page/index'
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :ad do
+    get 'dashboard/index'
+    root 'dashboard#index'
+  end
+
+  get 'page/index'
+  root to: 'page#index'
+
 end
