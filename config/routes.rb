3446a00d6b56762f866c1209c387a73c03acb045
Diego Collares <diego.collares@gmail.com>
2017-03-16 23:14:03 -0300

    add resorce vehicles

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
 
+
   devise_for :admins
 
   namespace :ad do
     get 'dashboard/index'
     root 'dashboard#index'
+    resources :vehicles
   end
 
   get 'page/index'
