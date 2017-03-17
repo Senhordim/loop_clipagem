ceb70492535d78164e805743494a43555d92a9c0
Diego Collares <diego.collares@gmail.com>
2017-03-16 23:07:47 -0300

    call lib ad

--- app/assets/javascripts/ad.js
+++ app/assets/javascripts/ad.js
@@ -2,9 +2,13 @@
 //= require jquery.turbolinks
 //= require jquery_ujs
 //= require bootstrap_sb_admin_base_v2
+//= require bootstrap-select
+//= require bootstrap-toggle
 //
 //  your script here
 //
 //= require turbolinks
+
+
 
 
