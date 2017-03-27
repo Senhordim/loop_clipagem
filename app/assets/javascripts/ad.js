80477117457276370074f4ae93a943586f14168e
Diego Collares <diego.collares@gmail.com>
2017-03-26 23:11:17 -0300

    update layout and add alert-boostrap

--- app/assets/javascripts/ad.js
+++ app/assets/javascripts/ad.js
@@ -4,10 +4,13 @@
 //= require bootstrap_sb_admin_base_v2
 //= require bootstrap-select
 //= require bootstrap-toggle
+//= require ad/admin
 //
 //  your script here
 //
 //= require turbolinks
+
+
 
 
 
