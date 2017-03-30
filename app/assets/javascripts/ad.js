f6f293551cd4023489db03d46a0e058339842c01
Diego Collares <diego.collares@gmail.com>
2017-03-30 17:47:19 -0300

    import ichek'

--- app/assets/javascripts/ad.js
+++ app/assets/javascripts/ad.js
@@ -4,6 +4,9 @@
 //= require bootstrap_sb_admin_base_v2
 //= require bootstrap-select
 //= require bootstrap-toggle
+//= require chosen-jquery
+//= require 'icheck'
+
 //= require ad/admin
 //
 //  your script here
