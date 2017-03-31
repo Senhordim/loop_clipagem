898d4defc2a3dc7ba934448e67c3b0a935853e35
Diego Collares <diego.collares@gmail.com>
2017-03-30 21:05:49 -0300

    remove icheck

--- app/assets/javascripts/ad/admin.js
+++ app/assets/javascripts/ad/admin.js
@@ -12,23 +12,6 @@ $(function() {
 });
 
 
-$(".chosen-select").chosen({no_results_text: "Oops, nothing found!"});
+$(".chosen-select").chosen({no_results_text: "Oops, não encontrado!"});
 
-function icheck(){
-  if($(".icheck-me").length > 0){
-    $(".icheck-me").each(function(){
-      var $el = $(this);
-      var skin = ($el.attr('data-skin') !== undefined) ? "_" + $el.attr('data-skin') : "",
-      color = ($el.attr('data-color') !== undefined) ? "-" + $el.attr('data-color') : "";
-      var opt = {
-        checkboxClass: 'icheckbox' + skin + color,
-        radioClass: 'iradio' + skin + color,
-      }
-      $el.iCheck(opt);
-    });
-  }
-}
 
-$(function(){
-  icheck();
-})
