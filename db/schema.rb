21ccbf10e4aa06d161a593f9196f8afe9b50c1d7
Diego Collares <diego.collares@gmail.com>
2017-04-04 11:15:32 -0300

    change schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170330205408) do
+ActiveRecord::Schema.define(version: 20170403230920) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,8 +40,9 @@ ActiveRecord::Schema.define(version: 20170330205408) do
     t.integer  "page"
     t.string   "link"
     t.integer  "vehicle_id"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",          null: false
+    t.datetime "updated_at",          null: false
+    t.date     "date_of_publication"
     t.index ["vehicle_id"], name: "index_publications_on_vehicle_id", using: :btree
   end
 
