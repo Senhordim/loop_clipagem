c4eba473eeacfcbddc69fbdf2a6650297cc69d7f
Diego Collares <diego.collares@gmail.com>
2017-04-05 12:49:20 -0300

    remove lib

--- app/assets/javascripts/ad.js
+++ app/assets/javascripts/ad.js
@@ -2,8 +2,6 @@
 //= require jquery.turbolinks
 //= require jquery_ujs
 //= require bootstrap_sb_admin_base_v2
-//= require bootstrap-select
-//= require bootstrap-toggle
 //= require chosen-jquery
 //= require summernote
 //= require bootstrap-datepicker
