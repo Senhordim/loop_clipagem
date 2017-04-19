e49bfd216d5bb63b4c3402eada22b10b3d20a76f
Diego Collares <diego.collares@gmail.com>
2017-04-19 16:27:59 -0300

    created cntant with ESTADOS_BRASILEIROS

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,20 +1,37 @@
 module ApplicationHelper
 
-  def bootstrap_class_for flash_type
-    { success: "alert-success", error: "alert-danger", alert: "alert-warning", notice: "alert-info" }[flash_type.to_sym] || flash_type.to_s
-  end
+  ESTADOS_BRASILEIROS = [
+    ["Acre", "AC"],
+    ["Alagoas", "AL"],
+    ["Amapá", "AP"],
+    ["Amazonas", "AM"],
+    ["Bahia", "BA"],
+    ["Ceará", "CE"],
+    ["Distrito Federal", "DF"],
+    ["Espírito Santo", "ES"],
+    ["Goiás", "GO"],
+    ["Maranhão", "MA"],
+    ["Mato Grosso", "MT"],
+    ["Mato Grosso do Sul", "MS"],
+    ["Minas Gerais", "MG"],
+    ["Pará", "PA"],
+    ["Paraíba", "PB"],
+    ["Paraná", "PR"],
+    ["Pernambuco", "PE"],
+    ["Piauí", "PI"],
+    ["Rio de Janeiro", "RJ"],
+    ["Rio Grande do Norte", "RN"],
+    ["Rio Grande do Sul", "RS"],
+    ["Rondônia", "RO"],
+    ["Roraima", "RR"],
+    ["Santa Catarina", "SC"],
+    ["São Paulo", "SP"],
+    ["Sergipe", "SE"],
+    ["Tocantins", "TO"]
+  ]
 
-  def flash_messages(opts = {})
-    flash.each do |msg_type, message|
-      concat(content_tag(:div, message, class: "alert #{bootstrap_class_for(msg_type)} alert-dismissible", role: 'alert') do
-        concat(content_tag(:button, class: 'close', data: { dismiss: 'alert' }) do
-          concat content_tag(:span, '&times;'.html_safe, 'aria-hidden' => true)
-          concat content_tag(:span, 'Close', class: 'sr-only')
-        end)
-        concat message
-      end)
-    end
-    nil
+  def options_for_states(selected)
+    options_for_select(ESTADOS_BRASILEIROS, selected)
   end
 
 end
