6077ac07d58b123ba8a14564214f350342a8270d
Diego Collares <diego.collares@gmail.com>
2017-04-24 19:08:08 -0300

    config routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,22 @@
 Rails.application.routes.draw do
 
+  devise_for :customers, skip: [:registrations]
   devise_for :admins, skip: [:registrations]
 
   namespace :ad do
+
     get 'dashboard/index'
     root 'dashboard#index'
+
+    resources :customers, except: [:show]
     resources :admins, except: [:show]
     resources :vehicles
     resources :publications
+
+  end
+
+  namespace :clipping do
+
   end
 
   namespace :site do
