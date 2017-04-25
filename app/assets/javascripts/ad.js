f7502f0cef29bdca32250610b9b1f3dcc03a1d1d
Diego Collares <diego.collares@gmail.com>
2017-04-25 14:46:36 -0300

    change layout ad and js call libs

--- app/assets/javascripts/ad.js
+++ app/assets/javascripts/ad.js
@@ -1,19 +1,16 @@
-//= require jquery
-//= require jquery.turbolinks
-//= require jquery_ujs
 //= require bootstrap_sb_admin_base_v2
-//= require chosen-jquery
 //= require summernote
+//= require chosen-jquery
 //= require bootbox
 //= require jquery.mask
+//= require bootstrap-datepicker
+//= require bootstrap-datepicker/locales/bootstrap-datepicker.pt-BR
 //= require mask
 //= require findAddress
 //= require bootbox-rails
 //= require mediaelement-and-player.min
-//= require bootstrap-datepicker
-//= require bootstrap-datepicker/locales/bootstrap-datepicker.pt-BR
 //= require app
-//= require turbolinks
+
 
 
 
--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,20 +1,25 @@
 $(document).ready(function(){
 
-  $('.datepicker').datepicker({
+  // datepicker
+  $('.date-picker').datepicker({
     format: 'dd/mm/yyyy',
     language: 'pt-BR'
   });
 
+  // summernote
   $('[data-provider="summernote"]').each(function(){
     $(this).summernote({
       height: 300
     });
   });
 
+  // Tooltip
   $('[data-toggle="tooltip"]').tooltip()
 
+  // Chosen select
   $(".chosen-select").chosen({no_results_text: "Oops, não encontrado!"});
 
+  // Flash alert
   $(function() {
     var flashCallback;
 
@@ -31,13 +36,12 @@ $(document).ready(function(){
     return setTimeout(flashCallback, 2500);
   });
 
-
+  // Media player
   $('video').mediaelementplayer({
     alwaysShowControls: false,
     videoVolume: 'horizontal',
     features: ['playpause','progress','volume','fullscreen']
   });
-
   $('audio').mediaelementplayer({
     alwaysShowControls: false,
     audioVolume: 'horizontal',
