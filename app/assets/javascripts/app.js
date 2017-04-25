d6a487b4b157216edcf0bc0e32806ac2c297c572
Diego Collares <diego.collares@gmail.com>
2017-04-25 14:13:47 -0300

    update app js

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,32 +1,36 @@
-$(function() {
+$(document).ready(function(){
+
+  $('.datepicker').datepicker({
+    format: 'dd/mm/yyyy',
+    language: 'pt-BR'
+  });
+
+  $('[data-provider="summernote"]').each(function(){
+    $(this).summernote({
+      height: 300
+    });
+  });
+
+  $('[data-toggle="tooltip"]').tooltip()
+
+  $(".chosen-select").chosen({no_results_text: "Oops, não encontrado!"});
+
+  $(function() {
     var flashCallback;
+
     flashCallback = function() {
         return $(".alert").fadeOut();
     };
+
     $(".alert").bind('click', (function(_this) {
       return function(ev) {
         return $(".alert").fadeOut();
       };
     })(this));
+
     return setTimeout(flashCallback, 2500);
-});
-
-$(".chosen-select").chosen({no_results_text: "Oops, não encontrado!"});
-
-$(function () {
-  $('[data-toggle="tooltip"]').tooltip()
-});
-
-$('[data-provider="summernote"]').each(function(){
-  $(this).summernote({
-    height: 300
   });
-});
 
-$(document).ready(function(){
-  $('.datepicker').datepicker({
-    format: 'dd/mm/yyyy',
-    language: 'pt-BR'
-  });
+
 });
 
