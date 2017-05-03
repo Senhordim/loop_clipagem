6b5af7a6e77880d0a6e725d22dcecc119e286fcb
Diego Collares <diego.collares@gmail.com>
2017-05-03 15:15:32 -0300

    created ad controller

--- app/controllers/ad/publications_controller.rb
+++ app/controllers/ad/publications_controller.rb
@@ -1,13 +1,13 @@
 class Ad::PublicationsController < AdController
 
   before_action :set_publication, only: [:show, :edit, :update, :destroy]
+  before_action :set_all_customer, only: [:new, :edit]
 
   def index
     @publications = Publication.order('created_at DESC' ).page params[:page]
   end
   def new
     @publication = Publication.new
-    @customers = Customer.all
   end
 
   def create
@@ -40,6 +40,10 @@ class Ad::PublicationsController < AdController
 
   private
 
+  def set_all_customer
+    @customers = Customer.all
+  end
+
   def set_publication
     @publication = Publication.find(params[:id])
   end
@@ -57,5 +61,5 @@ class Ad::PublicationsController < AdController
       customer_ids: []
     )
   end
-  
+
 end
