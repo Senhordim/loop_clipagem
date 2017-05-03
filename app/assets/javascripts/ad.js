8285060d6007ccf2020f02b4fe984ca822f51f3c
Diego Collares <diego.collares@gmail.com>
2017-05-03 15:30:09 -0300

    update js to project

--- app/assets/javascripts/ad.js
+++ app/assets/javascripts/ad.js
@@ -1,10 +1,11 @@
 //= require bootstrap_sb_admin_base_v2
+//= require bootstrap-datepicker
+//= require bootstrap-datepicker/locales/bootstrap-datepicker.pt-BR
 //= require summernote
 //= require chosen-jquery
 //= require bootbox
 //= require jquery.mask
-//= require bootstrap-datepicker
-//= require bootstrap-datepicker/locales/bootstrap-datepicker.pt-BR
+
 //= require mask
 //= require findAddress
 //= require bootbox-rails
